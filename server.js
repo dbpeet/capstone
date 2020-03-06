f6d8080fa4bc43228002d1b339e928b0ea11eba5
dylanpeet <dylanpeet@gmail.com>
2020-03-05 16:58:35 -0800

    Fix array, dammit

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.use(session({
 }));
 
 const corsOptions = {
-  origin: "https://capstone-frontend-artwork.herokuapp.com/", // string or array
-  allowedHeaders: "['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept']",
+  origin: "*", // string or array
+  allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept'],
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
   credentials: true, // This allows the session cookie to be sent back and forth
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
