c69638011ccf5b8ac51c4e9a5ec709a24c4e5d26
dylanpeet <dylanpeet@gmail.com>
2020-03-05 18:49:51 -0800

    geez

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
-
-app.use(cors())
+var corsOptions = {
+    origin: 'https://capstone-frontend-artwork.herokuapp.com',
+    allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept'],
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions))
 
 const PORT = process.env.PORT || 4000;
 
