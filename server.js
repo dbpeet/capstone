008e759350a96ba45c11553719091523b8aab967
dylanpeet <dylanpeet@gmail.com>
2020-03-05 17:26:32 -0800

    fuckit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(cors(corsOptions));
-app.options('*', cors());
+app.options('*', cors(corsOptions));
 
 
 const PORT = process.env.PORT || 4000;
