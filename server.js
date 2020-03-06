9694abaf9a7eccca2dce3b4c6e170785a7bc87e5
dylanpeet <dylanpeet@gmail.com>
2020-03-05 17:43:04 -0800

    all cors

--- server.js
+++ server.js
@@ -7,12 +7,7 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
-
+app.use(cors())
 const PORT = process.env.PORT || 4000;
 
 // ---------------------------------------- MIDDLEWARE ---------------------------------------- //
